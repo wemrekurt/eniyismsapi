6942ac4ac88a8e73341475ba4ca26ee06a7fed09
wemrekurt <emre.kurt@bil.omu.edu.tr>
2016-08-09 13:56:20 +0300

    Send One Receiver Tamamlandi

--- test/dummy/app/controllers/smstest_controller.rb
+++ test/dummy/app/controllers/smstest_controller.rb
@@ -10,5 +10,13 @@ class SmstestController < ApplicationController
     bal = Eniyismsapi::REPORT.new '5444059964', '123456'
     @b = bal.balance
 
+    sen = Eniyismsapi::SMS.new '5444059964', '123456'
+    params = {
+      coding: 'Default',
+      originator: 'EMRE KURT',
+      message: 'Ruby Gem Test API',
+      numbers: ['5444059964']
+    }
+    @send = sen.send params
   end
 end
